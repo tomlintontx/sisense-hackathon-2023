75a16411a6272dba9f50cbe9e213045c2aa865fe
tomlintontx <tomlintontx@gmail.com>
2023-09-18 09:43:25 -0500

    added new team player card

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar.tsx'
 import PricingCardsList from './components/PricingCardsList.tsx'
 import { Container, Box } from '@mui/material';
 import { PricingCardProps } from './components/PricingCard.tsx';
+import { Player } from './components/PlayerCard.tsx';
+import PlayerCard from './components/PlayerCard.tsx';
 
 const data: PricingCardProps[] = [
   {
@@ -35,13 +37,20 @@ const data: PricingCardProps[] = [
   }
 ]
 
+const playerData: Player = {
+  name: 'Tom Brady',
+  position: 'Quarterback',
+  imageUrl: 'https://media.cnn.com/api/v1/images/stellar/prod/221028100056-brady-sacked-tease.jpg?c=original',
+  stats: [
+    { label: 'Passing Yards', value: '4633' },
+    { label: 'Touchdowns', value: '40' },
+    { label: 'Interceptions', value: '12' },
+    // ... Add more stats as needed
+  ],
+};
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <Navbar />
-      <Container>
-        <Box display="flex" justifyContent="center" mt={10}> {/* Added some margin to give space below navbar */}
-        <PricingCardsList cardsData={data} />
-        </Box>
-      </Container>
+    <PlayerCard player={playerData} />
   </React.StrictMode>,
 )
